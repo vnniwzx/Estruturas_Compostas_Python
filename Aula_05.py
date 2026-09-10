a0642db2199b7f39b3fb3b847e574eb4eae19b6a
Vinicius Furtado <viniciusfurts@gmail.com>
2026-09-10 08:41:00 -0300

    Add inventory and cities data with display logic

---
+++ Aula_05.py
@@ -0,0 +1,19 @@
+inventario = [
+    ("Espada", "arma"),
+    ("Poção de Cura", "poção")
+]
+ 
+for item in inventario:
+    print("Item:", item[0])
+    print("Tipo:", item[1])
+    print()
+ 
+cidades = [
+    (-23.5475,-46.6361),
+    (-25.44, -49.28)
+]
+ 
+for cidade in cidades:
+    print("Latitude: ", cidade[0])
+    print("longitude: ", cidade[1])
+    print()
