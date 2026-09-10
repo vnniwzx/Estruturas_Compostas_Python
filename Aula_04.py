e9b1be698d56da6a702ac88ceb3c08aec2e8723b
Vinicius Furtado <viniciusfurts@gmail.com>
2026-09-10 08:40:43 -0300

    Add personagem and carro dictionaries with inventory

---
+++ Aula_04.py
@@ -0,0 +1,23 @@
+personagem = {
+    "nome": "Arthos",
+    "classe": "Guerreiro",
+    "nivel": 1,
+    "inventario": []
+}
+ 
+personagem["inventario"].append("Espada")
+personagem["inventario"].append("Poção")
+ 
+print(personagem)
+ 
+carro = {
+    "nome": "BYD",
+    "modelo": "Dolphin",
+    "acessorios": []
+}
+ 
+carro["acessorios"].append("Suporte celular")
+carro["acessorios"].append("Tapetes")
+carro["acessorios"].append("Carregador veicular")
+ 
+print(carro)
